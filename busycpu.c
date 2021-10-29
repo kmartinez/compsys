f8592852fbb6ed2f8573a64b763136a544ba2b16
Kirk Martinez <km@ecs.soton.ac.uk>
2021-10-29 17:06:58 +0100

    more work in loop

--- busycpu.c
+++ busycpu.c
@@ -1,3 +1,7 @@
+/* simple code to do a lot of arithmetic loops
+* about 11 instr per loop on an ARM Cortex A53
+* when compiled with gcc -O
+*/
 #include <stdio.h>
 #define NUM 1000000000
 void main()
@@ -7,6 +11,9 @@ void main()
 	for(a = 0; a < NUM; a++)
 	{
 		b += a & 0x01;
+		b -= (a & 0x02);
+		b += (a & 0x04);
+		b -= (a & 0x08);
 	}
 	printf("%d\n",b);
 }

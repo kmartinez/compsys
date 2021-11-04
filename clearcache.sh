c1cbceb34db0f8980f131ef471bc40e8a775b7da
Kirk Martinez <km@ecs.soton.ac.uk>
2021-11-04 09:27:58 +0000

    even more info in comments

--- clearcache.sh
+++ clearcache.sh
@@ -1,2 +1,3 @@
-echo "clearing disk cache"
+echo "clearing the disk cache stored in the Pi's RAM"
+echo "if this fails you forgot to run it with sudo ./clearcache.sh or didn't set it as executable with chmod"
 echo 1 > /proc/sys/vm/drop_caches

ad1f0abb55203e5ca082e84e50402a2ccd874502
Kirk Martinez <km@ecs.soton.ac.uk>
2021-11-25 12:12:03 +0000

    more helpful comments

--- printint.s
+++ printint.s
@@ -14,8 +14,8 @@ push {lr}			@ save link register as we will re-use it here
 	ldr r1, =1000000
 	mov r0, #2
         mul     r0, r0, r1
-
-	@ print the result that we have stored in r0
+	@ end of our code
+	@ print the result that we have stored in r0 - don't change any code below
 	mov r1, r0		
 	ldr r0, =fmtstring
 	bl printf

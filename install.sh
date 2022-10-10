2eec34e07827ba8106fc6430c7c5970bc9ba33a2
Kirk Martinez <km@ecs.soton.ac.uk>
2022-10-10 14:14:16 +0100

    2022 version - spi serial and git check

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # COMP1203 Setup Script
-# ONLY to configure things/hardware on a fresh Raspbian
-# probably harmless otherwise but it will reboot the Pi
-# Laurie Kirkcaldy / Josh Curry / Kirk Martinez 2018
+# ONLY to configure things/hardware on a fresh Raspbian and check git files are OK
+# enables SPI, serial and download git files
+# Kirk Martinez 2022
 
-read -p "Are you sure you want to configure Raspbian hardware settings? (y/n) " answer
+read -p "Are you sure you want to configure Pi for comp1203? (y/n) " answer
 if [[ $answer != y ]]
 then
   echo "exiting without changing anything"
@@ -15,11 +14,17 @@ echo "enabling SPI, hardware serial"
 sudo raspi-config nonint do_spi 0 # enable SPI
 sudo raspi-config nonint do_serial 1 # disable linux serial, enable hardware serial
 sudo sed -i -e "s/enable_uart=0/enable_uart=1/" /boot/config.txt
-echo "checking compy1203 python is up to date"
-cd ~/comp1203-python
-git pull
-git checkout master
-pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/fjord.jpg
-echo "Rebooting in 3 seconds. CTRL-C to interrupt."
-sleep 3
-sudo reboot
+echo "checking comp1203 files are up to date"
+cd /home/pi
+if [ -d comp1203 ]; then
+	echo "comp1203 files exist - updating"
+	cd comp1203
+	git pull
+	cd ..
+else
+	echo "getting comp1203 files"
+	git clone https://github.com/kmartinez/comp1203.git
+fi
+
+pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/RPiSystem.png
+echo "you should Reboot with sudo reboot"

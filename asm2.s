5ed6b80f695d81a8ab7e4bf64580a2e3dbb681c4
Kirk Martinez <km@ecs.soton.ac.uk>
2023-11-20 15:33:43 +0000

    Update asm2.s

--- asm2.s
+++ asm2.s
@@ -1,28 +1,27 @@
 # a simple loop
 # adding up the counter from 1 to 5
-# return value to shell for printing with echo $?
-@ comp1203 template code
+# comp1203 template code
 
 .data
-    .balign  4
-    fmtstring: .asciz "%d\n"
+.balign  4
+fmtstring: .asciz "%d\n"
 .text
-    .global main
-    .extern printf
+.global main
+.extern printf
 
 main:
 push {lr}			@ save link register as we will re-use it here
 @ our code here
-    mov r1, #0       /* r1 ← 0 is our sum */
-    mov r2, #1       /* r2 ← 1  our counter */
+    mov r1, #0       @ r1 ← 0 is our sum 
+    mov r2, #1       @ r2 ← 1  our counter
 loop: 
-    cmp r2, #5      /* compare r2 to our limit*/
-    bgt end          /* branch if r2 > limit to end */
-    add r1, r1, r2   /* r1 ← r1 + r2 */
-    add r2, r2, #1   /* r2 ← r2 + 1 */
+    cmp r2, #5       @ compare r2 to our limit
+    bgt end          @ branch if r2 > limit to end 
+    add r1, r1, r2   @ r1 ← r1 + r2 
+    add r2, r2, #1   @ r2 ← r2 + 1
     b loop
 end:
-    mov r0, r1       /* r0 ← r1 */
+    mov r0, r1       @ r0 ← r1
 
 	@ print the result that we have stored in r0
 	mov r1, r0		

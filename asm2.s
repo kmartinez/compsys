6847a2ae82c4811d6aa59d171db94d276bcdaaa5
Kirk Martinez <km@ecs.soton.ac.uk>
2023-11-28 09:24:21 +0000

    Update asm2.s

--- asm2.s
+++ asm2.s
@@ -1,5 +1,6 @@
 # a simple loop
 # adding up the counter from 1 to 5
+# print takes two args: r0 (format) and r1 (the value you want to print)
 # comp1203 template code
 
 .data
@@ -10,21 +11,18 @@ fmtstring: .asciz "%d\n"
 .extern printf
 
 main:
-push {lr}			@ save link register as we will re-use it here
+push {lr}			@ save link register as we use it
 @ our code here
-    mov r1, #0       @ r1 ← 0 is our sum 
-    mov r2, #1       @ r2 ← 1  our counter
+	mov r1, #0       @ r1 ← 0 is our sum 
+	mov r2, #1       @ r2 ← 1  our counter
 loop: 
-    cmp r2, #5       @ compare r2 to our limit
-    bgt end          @ branch if r2 > limit to end 
-    add r1, r1, r2   @ r1 ← r1 + r2 
-    add r2, r2, #1   @ r2 ← r2 + 1
-    b loop
+	cmp r2, #5       @ compare r2 to our limit
+	bgt end          @ branch if r2 > limit to end 
+	add r1, r1, r2   @ r1 ← r1 + r2 
+	add r2, r2, #1   @ r2 ← r2 + 1
+	b loop
 end:
-    mov r0, r1       @ r0 ← r1
-
-	@ print the result that we have stored in r0
-	mov r1, r0		
+	@ print value of r1
 	ldr r0, =fmtstring
 	bl printf
 
